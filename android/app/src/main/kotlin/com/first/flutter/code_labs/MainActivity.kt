594c8ea7d1654ca45d92b90e90021ccd22bdbe4e
Serhii Dymenko <serge.sergeevich@gmail.com>
2019-10-28 20:32:09 +0200

    Initial Commit

---
+++ android/app/src/main/kotlin/com/first/flutter/code_labs/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.first.flutter.code_labs
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
