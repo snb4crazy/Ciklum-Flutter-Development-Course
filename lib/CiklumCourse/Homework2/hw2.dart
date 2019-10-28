940ccf921a9a57bbe269f73b7e95845530b16858
Serhii Dymenko <serge.sergeevich@gmail.com>
2019-10-29 01:20:53 +0200

    Lecture #2: Homework Main

--- lib/CiklumCourse/Homework2/hw2.dart
+++ lib/CiklumCourse/Homework2/hw2.dart
@@ -1,8 +1,17 @@
-/*
-Ciklum Flutter Course hw #2
-https://flutter.dev/docs/codelabs/layout-basics
-Tested in DartPad - https://dartpad.dev/
- */
-
 import 'package:flutter/material.dart';
 
+List<String> items = ['Item 1', 'Item 2', 'Item 3'];
+
+class TodoList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return TodoListState();
+  }
+}
+
+class TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
