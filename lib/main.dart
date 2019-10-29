f199454f5d39f7b07380012075c12970f179d3f6
Serhii Dymenko <serge.sergeevich@gmail.com>
2019-10-29 16:08:09 +0200

    CodeLab #3:Chat: Build Screen

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,22 @@ class GoogleCodeLab3Chat extends StatelessWidget {
     );
   }
 }
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Friendlychat",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat")),
+    );
+  }
+}
