436713bc2fc453e8b5ca110ff164d38c336ea8f2
Serhii Dymenko <serge.sergeevich@gmail.com>
2019-11-04 01:14:22 +0200

    CiklumCourse:Image App: AppBar Theme

--- lib/CiklumCourse/Homework3/grid_image_page.dart
+++ lib/CiklumCourse/Homework3/grid_image_page.dart
@@ -9,12 +9,43 @@ class GridImagePage extends StatelessWidget {
 
   final String title = 'Grid View';
 
+  _getAppBar () {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+        textTheme: _getAppBarThemeData(),
+        actions: <Widget>[
+          Icon(Icons.settings),
+        ],
+        leading: Icon(Icons.menu),
+      ),
+    );
+  }
+
+  _getAppBarThemeData() {
+    return ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+
+        // Define the default font family.
+        fontFamily: 'Montserrat',
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+        headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+    title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+    body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+    ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(this.title),
-        ),
+        appBar: _getAppBar(),
         body: GridView.count(
           crossAxisCount: 2,
           padding: EdgeInsets.all(5),
