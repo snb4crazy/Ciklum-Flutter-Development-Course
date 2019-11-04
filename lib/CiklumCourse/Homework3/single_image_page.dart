e5625cec5b8ef84966b7bf96c4f0c056eab56a2b
Serhii Dymenko <serge.sergeevich@gmail.com>
2019-11-04 02:55:59 +0200

    CiklumCourse:Image App: Image Picker Integration

--- lib/CiklumCourse/Homework3/single_image_page.dart
+++ lib/CiklumCourse/Homework3/single_image_page.dart
@@ -2,10 +2,14 @@
 Unused Class for testing only
 Testing Homework:
     2. Show an image from the internet by URL.
+    1. "Add" button on the list screen and add a new image by a tap
  */
 
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:image_picker_modern/image_picker_modern.dart';
+import 'dart:async';
+import 'dart:io';
 
 class SingleImagePage extends StatefulWidget {
   SingleImagePage({Key key, this.title}) : super(key: key);
@@ -18,6 +22,16 @@ class SingleImagePage extends StatefulWidget {
 
 class _SingleImagePageState extends State<SingleImagePage> {
 
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,13 +42,20 @@ class _SingleImagePageState extends State<SingleImagePage> {
         children: [
           Center(child: CircularProgressIndicator()),
           Center(
-            child: FadeInImage.memoryNetwork(
+            child: _image == null
+                ? FadeInImage.memoryNetwork(
               placeholder: kTransparentImage,
               image: 'https://ae01.alicdn.com/kf/HTB1q3WgbIUrBKNjSZPxq6x00pXae/Funny-Dog-Cat-Costumes-High-School-Uniform-Cosplay-Suit-Pet-Apparel-Halloween-Christmas-Clothes-For-Puppy.jpg_q50.jpg',
-            ),
+            )
+                : Image.file(_image),
           ),
         ],
         ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
     ); // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
