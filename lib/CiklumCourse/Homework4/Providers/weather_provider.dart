0b96d4ed7b9f06698eb965edab46bc360a897b4b
Serhii Dymenko <serge.sergeevich@gmail.com>
2019-11-04 04:09:34 +0200

    CiklumCourse:Weather Async App: Validate HTTP Response

--- lib/CiklumCourse/Homework4/Providers/weather_provider.dart
+++ lib/CiklumCourse/Homework4/Providers/weather_provider.dart
@@ -4,15 +4,21 @@ Weather Provider - Request API with HTTP package
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:code_labs/CiklumCourse/Homework4/Models/weather.dart';
 
 class WeatherProvider {
 
   final weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Kharkiv&units=metric&APPID=1ea55013049215603ece3fee22806975';
 
-  Future<http.Response> getCurrentWeather() async {
+  Future<Weather> getCurrentWeather() async {
     final response = await http.get(this.weatherUrl);
     final statusCode = response.statusCode;
     final responseBody = response.body;
-    return response;
+    if (statusCode == 200) {
+      return Weather.fromJson(json.decode(responseBody));
+    } else {
+      throw Exception('Failed to load weather data');
+    }
   }
 }
