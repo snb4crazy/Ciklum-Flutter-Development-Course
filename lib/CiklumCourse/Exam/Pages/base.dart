d5d5a89eb5606dc39bfb90fdd9ec95f5720d9c31
Serhii Dymenko <serge.sergeevich@gmail.com>
2019-11-07 19:21:16 +0200

    CiklumCourse:Exam: Base Page

---
+++ lib/CiklumCourse/Exam/Pages/base.dart
@@ -0,0 +1,99 @@
+/*
+Base Page of the "Almost Tinder" app
+ */
+
+import 'package:flutter/material.dart';
+import 'package:code_labs/CiklumCourse/Exam/Pages/details.dart';
+
+class RandomUserPage extends StatelessWidget {
+
+  //todo migrate to StatefulWidget
+
+  final String title = 'Almost Tinder';
+
+  _getAppBar () {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+        textTheme: _getAppBarThemeData(),
+        actions: <Widget>[
+          Icon(Icons.settings),
+        ],
+        leading: Icon(Icons.menu),
+      ),
+    );
+  }
+
+  _getAppBarThemeData() {
+    return ThemeData(
+      // Define the default brightness and colors.
+      brightness: Brightness.dark,
+      primaryColor: Colors.lightBlue[800],
+      accentColor: Colors.cyan[600],
+
+      // Define the default font family.
+      fontFamily: 'Montserrat',
+
+      // Define the default TextTheme. Use this to specify the default
+      // text styling for headlines, titles, bodies of text, and more.
+      textTheme: TextTheme(
+        headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+        title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+        body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+      ),
+    );
+  }
+
+  _getBaseLayout() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.account_circle, size: 50)),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                //todo put image
+              ],
+            ),
+          ],
+        ),
+        SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text ('Name of Random User'),
+          ],
+        ),
+        SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          //todo replace icons with buttons
+          children: [
+            Icon (
+              //todo implement tapping
+              Icons.refresh,
+            ),
+            //todo implement tapping
+            Icon (
+              Icons.skip_next,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _getAppBar(),
+      body: _getBaseLayout(),
+    );
+  }
+}
