c861df48af95ca17fdbc1af5668bec38ad93c431
Serhii Dymenko <serge.sergeevich@gmail.com>
2019-11-07 20:22:39 +0200

    CiklumCourse:Exam: Implement Distance Calculation

--- lib/CiklumCourse/Exam/Models/user.dart
+++ lib/CiklumCourse/Exam/Models/user.dart
@@ -5,11 +5,15 @@ Source: randomuser.me
 Data needed: Image, Name, GPS coordinates (?)
  */
 
+import 'package:latlong/latlong.dart';
+
 class User {
   final String name;
   final String imageUrl;
-  final int latitude;
-  final int longitude;
+  final double latitude;
+  final double longitude;
+  //todo for testing only set phone coordinates to 0
+  final phoneCoordinates = {'lat': 0.0, 'long': 0.0};
 
   User(this.name, this.imageUrl, this.latitude, this.longitude);
 
@@ -19,7 +23,16 @@ class User {
         latitude = json['results']['location'] ['coordinates'] ['latitude'],
         longitude = json['results']['location'] ['coordinates'] ['longitude'];
 
-  //todo method calc geoposition by lat/long
+  double getDistance () {
+
+    final Distance distance = Distance();
+
+    final double km = distance.as(LengthUnit.Kilometer,
+        LatLng(phoneCoordinates ['lat'], phoneCoordinates ['long']),
+        LatLng(latitude,longitude));
+
+    return km;
+  }
 
   //todo method calc distance from phone to user
 
